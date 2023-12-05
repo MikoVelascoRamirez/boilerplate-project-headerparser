cef01b2e206048e08ee19ac538a91f5da2bb5d8e
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2023-12-05 22:13:52 +0000

    feat: writing exercise solution
    Sending response object with indicated raw headers using a destructuring converting raw headers array into an object.

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  const {21 : ipaddress, 9 : language, 3 : software} = {...Object.values(req.rawHeaders)};
+  res.json({
+    ipaddress,
+    language,
+    software
+  })
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
